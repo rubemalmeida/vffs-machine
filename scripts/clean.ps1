543158e1b1cefc30ea4058fbc952e7d76a24900b
rubemalmeida <ruvalmeida+github.com@gmail.com>
2024-02-02 03:04:42 -0300

    clean

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,6 +1,6 @@
 (Get-Content vffs-webserver.ino) |
 ForEach-Object {
-    $_ -replace '^(.*)(const String hostname = ).*$', '${1}${2}SECRET_HOSTNAME' `
-        -replace '^(.*)(const char \*ssid = ).*$', '${1}${2}SECRET_SSID' `
-        -replace '^(.*)(const char \*password = ).*$', '${1}${2}SECRET_WIFI_PASSWORD'
+    $_ -replace '^(.*)(const String hostname = ).*$', '${1}${2}SECRET_HOSTNAME;' `
+        -replace '^(.*)(const char \*ssid = ).*$', '${1}${2}SECRET_SSID;' `
+        -replace '^(.*)(const char \*password = ).*$', '${1}${2}SECRET_WIFI_PASSWORD;'
 } | Set-Content vffs-webserver.ino
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -e '59s/.*/const String hostname = SECRET_HOSTNAME/' -e '60s/.*/const char *ssid = SECRET_SSID/' -e '61s/.*/const char *password = SECRET_WIFI_PASSWORD/'
+sed -e '59s/.*/const String hostname = SECRET_HOSTNAME;/' -e '60s/.*/const char *ssid = SECRET_SSID;/' -e '61s/.*/const char *password = SECRET_WIFI_PASSWORD;/'
